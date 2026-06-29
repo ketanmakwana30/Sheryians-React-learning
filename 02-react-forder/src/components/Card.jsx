b3952a32e470ce74fc99675154bddcddbedf8d0f
ketanmakwana30 <ketanmakwana0130@gmail.com>
2026-06-29 15:58:35 +0530

    create react folder

---
+++ 02-react-forder/src/components/Card.jsx
@@ -0,0 +1,13 @@
+import "../index.css";
+
+const Card = () => {
+  return (
+    <div className="card">
+      <h1>Ketan Makwana</h1>
+      <p>Software Developer</p>
+      <small>This is guy to create amazing websites.</small>
+    </div>
+  );
+};
+
+export default Card;
---
+++ 02-react-forder/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <h1>Navbar </h1>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contect</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
